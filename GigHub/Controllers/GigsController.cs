302480e4128e2bdd8c18c9d05edfcb6e04f8caef
Skylock <emanuil.copil@gmail.com>
2018-10-16 14:16:46 +0300

    Update create view

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using GigHub.Data;
+using GigHub.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // Get: Gigs
+        public IActionResult Create()
+        {
+            var viewModel = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
