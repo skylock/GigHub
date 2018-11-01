1b485a53deb80f003135caa8163b1f1ecf3ebb40
Skylock <emanuil.copil@temasoft.com>
2018-11-01 15:48:05 +0200

    Enable BrowserLink

--- GigHub/Startup.cs
+++ GigHub/Startup.cs
@@ -56,6 +56,7 @@ namespace GigHub
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
                 app.UseDatabaseErrorPage();
             }
             else
