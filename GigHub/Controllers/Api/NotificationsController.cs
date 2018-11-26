bb4c797498bc8d61c1223772a4977992b4b743c5
Skylock <emanuil.copil@temasoft.com>
2018-11-26 08:03:36 +0200

    Introducing Dto's

---
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,53 @@
+﻿using GigHub.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace GigHub.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NotificationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<NotificationDto> GetNewNotifications()
+        {
+            var userId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == userId)
+                .Select(un => un.Notification)
+                .Include(n => n.Gig.Artist)
+                .ToList();
+
+            return notifications.Select(n => new NotificationDto
+            {
+                DateTime = n.DateTime,
+                Gig = new GigDto
+                {
+                    Artist = new UserDto
+                    {
+                        Id = n.Gig.Artist.Id,
+                        Name = n.Gig.Artist.FullName
+                    },
+                    DateTime = n.Gig.DateTime,
+                    Id = n.Gig.Id,
+                    IsCanceled = n.Gig.IsCanceled,
+                    Venue = n.Gig.Venue
+                },
+                OriginalDateTime = n.OriginalDateTime,
+                OriginalVenue = n.OriginalVenue,
+                Type = n.Type
+            });
+        }
+    }
+}
---
+++ GigHub/Controllers/Api/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GigHub.Controllers.Api
+{
+    public class UserDto
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
