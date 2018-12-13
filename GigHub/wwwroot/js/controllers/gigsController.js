a24346df76f9f4e830b67dd7503fff1f39bf20b7
Skylock <emanuil.copil@gmail.com>
2018-12-13 16:42:38 +0200

    Optimize js code

---
+++ GigHub/wwwroot/js/controllers/gigsController.js
@@ -0,0 +1,36 @@
+﻿var GigsController = function (attendanceService) {
+    var button;
+
+    var fail = function (jqXhr) {
+        alert(jqXhr.responseJSON);
+    };
+
+    var done = function () {
+        var buttonText = button.text() === "Going" ? "Going ?" : "Going";
+
+        $(button)
+            .toggleClass("btn-info")
+            .toggleClass("btn-default")
+            .text(buttonText);
+
+    };
+
+    var toggleAttendance = function (e) {
+        button = $(e.target);
+
+        var gigId = button.attr("data-gig-id");
+
+        if (button.hasClass("btn-default"))
+            attendanceService.createAttendance(gigId, done, fail);
+        else
+            attendanceService.deleteAttendance(gigId, done, fail);
+    };
+
+    var init = function (container) {
+        $(container).on("click", ".js-toggle-attendance", toggleAttendance);
+    };
+
+    return {
+        init: init
+    };
+}(AttendanceService);
