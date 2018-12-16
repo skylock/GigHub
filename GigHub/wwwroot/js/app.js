de6f2c58af57098520a83160d4d3888a9908a9c9
Skylock <emanuil.copil@gmail.com>
2018-12-16 17:09:49 +0200

    Move notification data access code into a service

--- GigHub/wwwroot/js/app.js
+++ GigHub/wwwroot/js/app.js
@@ -20,6 +20,15 @@ var AttendanceService = function () {
         deleteAttendance: deleteAttendance
     };
 }();
+var NotificationService = function() {
+    var getNew = function (onDone) {
+        $.getJSON("/api/notifications/getNewNotifications", onDone);
+    };
+
+    return {
+        getNew     : getNew
+    };
+}();
 var GigsController = function (attendanceService) {
     var button;
 
@@ -56,40 +65,52 @@ var GigsController = function (attendanceService) {
         init: init
     };
 }(AttendanceService);
-var Notifications = function () {
+var Notifications = function (notificationService) {
+
+    var removeCountBadge = function() {
+        $(".js-notifications-count")
+            .text("")
+            .addClass("hide");
+    };
+
+    var fail = function() {
+        alert("Failed to mark as read!");
+    };
+
+    var showPopover = function(notifications) {
+        if (notifications.length === 0)
+            return;
+
+        $(".js-notifications-count")
+            .text(notifications.length)
+            .removeClass("hide")
+            .addClass("animated bounceInDown");
+
+        $(".notifications").popover({
+            html: true,
+            title: "Notifications",
+            content: function() {
+                const template = $("#notification-template").html();
+                const compiled = _.template(template);
+                return compiled({ notifications: notifications });
+            },
+            placement: "bottom",
+            template:
+                '<div class="popover popover-notifications" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>'
+        }).on("show.bs.popover",
+            function() {
+                $.post("/api/notifications/markAsRead")
+                    .done(removeCountBadge)
+                    .fail(fail);
+            });
+    };
 
     var getNew = function () {
-        $.getJSON("/api/notifications/getNewNotifications", function (notifications) {
-            if (notifications.length === 0)
-                return;
-
-            $(".js-notifications-count")
-                .text(notifications.length)
-                .removeClass("hide")
-                .addClass("animated bounceInDown");
-
-            $(".notifications").popover({
-                html: true,
-                title: "Notifications",
-                content: function () {
-                    const template = $("#notification-template").html();
-                    const compiled = _.template(template);
-                    return compiled({ notifications: notifications });
-                },
-                placement: "bottom",
-                template: '<div class="popover popover-notifications" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>'
-            }).on("show.bs.popover", function () {
-                $.post("/api/notifications/markAsRead")
-                    .done(function () {
-                        $(".js-notifications-count")
-                            .text("")
-                            .addClass("hide");
-                    });
-            });
-        });
+        notificationService.getNew(showPopover);
     };
 
     return {
         getNew: getNew
     };
-}();
+
+}(NotificationService);
---
+++ GigHub/wwwroot/js/services/notificationService.js
@@ -0,0 +1,9 @@
+﻿var NotificationService = function() {
+    var getNew = function (onDone) {
+        $.getJSON("/api/notifications/getNewNotifications", onDone);
+    };
+
+    return {
+        getNew     : getNew
+    };
+}();
