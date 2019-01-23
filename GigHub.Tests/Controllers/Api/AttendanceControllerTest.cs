788ea2c845e39ef12fc567736bbc50ff2b06b4c8
Skylock <emanuil.copil@gmail.com>
2019-01-23 15:26:27 +0200

    Test cancel and attended gig returns proper result

--- GigHub.Tests/Controllers/Api/AttendanceControllerTest.cs
+++ GigHub.Tests/Controllers/Api/AttendanceControllerTest.cs
@@ -40,7 +40,7 @@ namespace GigHub.Tests.Controllers.Api
         [TestMethod]
         public void Attend_GigIsAttended_ShouldReturnBadRequest()
         {
-            var attendance = _mockUnitOfWork.Setup(u => u.Attendances.GetAttendance(1, _userId))
+            _mockUnitOfWork.Setup(u => u.Attendances.GetAttendance(1, _userId))
                 .Returns(new Attendance
                 {
                     GigId = 1,
@@ -50,6 +50,21 @@ namespace GigHub.Tests.Controllers.Api
             var result = _sut.Attend(new AttendanceDto { GigId = 1 });
 
             result.Should().BeOfType<BadRequestObjectResult>();
+        }
+
+        [TestMethod]
+        public void Cancel_AttendedGig_ShouldReturnOk()
+        {
+            _mockUnitOfWork.Setup(u => u.Attendances.GetAttendance(1, _userId))
+                .Returns(new Attendance
+                {
+                    GigId = 1,
+                    AttendeeId = _userId
+                });
+
+            var result = _sut.Cancel(1);
+
+            result.Should().BeOfType<OkObjectResult>();
         }
     }
 }
